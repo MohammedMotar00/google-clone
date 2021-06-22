a258ceb4c57299a39a2a9cca8c13941bcff69cfd
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-06-22 19:31:19 +0200

    updated

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
     <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
       <div className="px-8 py-3">
         <p>Sverige</p>
+        <h1 className="text-center text-2xl text-black font-bold">This is Google clone app and I will use it for my portfolio</h1>
       </div>
 
       <div className="grid grid-cols-1 minMd:grid-cols-2 minLg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
